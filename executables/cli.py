8b37584898be655fe5ede01472335bf4d0cb8992
Kim <2727165@student.vu.nl>
2023-01-12 17:12:52 +0100

    Fixing spelling errors and long lines of code, for better readability

--- executables/cli.py
+++ executables/cli.py
@@ -8,7 +8,7 @@ from schnapsen.bots import MLDataBot, train_ML_model, MLPlayingBot, bully_bot
 from schnapsen.bots.example_bot import ExampleBot
 
 from schnapsen.game import (Bot, Move, PlayerPerspective,
-                            SchnapsenGamePlayEngine, Trump_Exchange)
+                            SchnapsenGamePlayEngine, TrumpExchange)
 from schnapsen.twenty_four_card_schnapsen import \
     TwentyFourSchnapsenGamePlayEngine
 
@@ -54,7 +54,7 @@ class NotificationExampleBot(Bot):
         print(f'result {"win" if won else "lost"}')
         print(f'I still have {len(state.get_hand())} cards left')
 
-    def notify_trump_exchange(self, move: Trump_Exchange) -> None:
+    def notify_trump_exchange(self, move: TrumpExchange) -> None:
         print(f"That trump exchanged! {move.jack}")
 
 
